750bb447491f48d352b8ed46afa6794adf255239
mmonteirorjbr <marcelo.sant.anna.monteiro@gmail.com>
2024-04-12 16:49:11 -0300

    1o commit

---
+++ src/aplicacao/ProgramaJogoXadrez.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+public class ProgramaJogoXadrez {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Hello World");
+	}
+
+}
