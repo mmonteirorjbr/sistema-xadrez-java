4b50796ac25471ff2aabeeda9eabb1eaf190d73b
mmonteirorjbr <marcelo.sant.anna.monteiro@gmail.com>
2024-05-10 14:21:49 -0300

    bishop

--- src/xadrez/PartidadeXadrez.java
+++ src/xadrez/PartidadeXadrez.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import tabuleiroJogo.Peca;
 import tabuleiroJogo.Posicao;
 import tabuleiroJogo.Tabuleiro;
+import xadrez.pecas.Bispo;
 import xadrez.pecas.Peao;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -289,8 +290,10 @@ public  PecadeXadrez[][] getPecas() {
     	 */
     	 
          PosicionaNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCA));
+         PosicionaNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCA));
  	     PosicionaNovaPeca('e', 1, new Rei(  tabuleiro, Cor.BRANCA));
-         PosicionaNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCA));
+ 	     PosicionaNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCA));
+	     PosicionaNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCA));
          PosicionaNovaPeca('a', 2, new Peao (tabuleiro, Cor.BRANCA));
          PosicionaNovaPeca('b', 2, new Peao( tabuleiro, Cor.BRANCA));
          PosicionaNovaPeca('c', 2, new Peao( tabuleiro, Cor.BRANCA));
@@ -301,8 +304,10 @@ public  PecadeXadrez[][] getPecas() {
          PosicionaNovaPeca('h', 2, new Peao( tabuleiro, Cor.BRANCA));
 
          PosicionaNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETA));
+         PosicionaNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETA));
  	     PosicionaNovaPeca('e', 8, new Rei(  tabuleiro, Cor.PRETA));
-         PosicionaNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETA));
+ 	     PosicionaNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETA));
+	     PosicionaNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETA));
          PosicionaNovaPeca('a', 7, new Peao( tabuleiro, Cor.PRETA));
          PosicionaNovaPeca('b', 7, new Peao( tabuleiro, Cor.PRETA));
          PosicionaNovaPeca('c', 7, new Peao( tabuleiro, Cor.PRETA));
